3651f0ffb75bfe4e8151aa493dabbfca97c2ecd7
Yeseul Lee <dev.yezlee@gmail.com>
2021-11-23 12:13:03 +0000

    task1,2

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const button = document.querySelector("#click-me");
 
+// Task 1 - The event object:
 function handleClick(event) {
   // If shift key pressed, change the inner text of the button to be NAILED IT
+  if (event.shiftKey) {
+    button.innerText = "NAILED IT";
+  }
 }
 
 button.addEventListener("click", handleClick);
@@ -9,3 +13,15 @@ button.addEventListener("click", handleClick);
 document.querySelector("#title-changer").addEventListener("keyup", (e) => {
   console.log(e.target.value);
 });
+
+// Task 2 - Adding an event listener
+let flowerButton = document.querySelector(".task-2");
+let title = document.querySelector("title");
+
+function flowerButtonFunction(e) {
+  title.innerText = "💐🌷🌼";
+}
+
+flowerButton.addEventListener("click", flowerButtonFunction);
+
+// Task 3 - Different events:
